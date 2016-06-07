4fb9ed827999599c02ef07dbf23df07f3f428c3e
cheuk <cochung1993@gmail.com>
2016-06-07 16:19:01 -0400

    kue_complete

--- app.js
+++ app.js
@@ -17,9 +17,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-var kue = require('kue')
-  , queue = kue.createQueue();
+var kue = require('kue');
+var queue = kue.createQueue({
+    disableSearch: false
+});
 
+
+
+
+
+
+var fs = require('fs');
+
+
+
+
+
+
+queue.on( 'error', function( err ) {
+  console.log( 'Oops... ', err );
+   console.log( 'Oops... ', err );
+    console.log( 'Oops... ', err );
+     console.log( 'Oops... ', err );
+      console.log( 'Oops... ', err );
+});
 
 
 
@@ -34,14 +55,96 @@ var spark_path = "/home/cochung/spark_full" ;
 
   var job_title = job.data.title
   var cmd_params = job.data.cmd_params
-  var log_cmd = " > "+job_title+".txt"
+
+  var job_file =job.id+".txt"
+  var log_cmd = " > "+job_file
 
 
-   exec(command1+ cmd_params+log_cmd,function(error, stdout, stderr){
-    if (stdout.length > 0) console.log('stdout: ' + stdout);
-    if (stderr.length > 0) console.log('stderr: ' + stderr);
-    done(error);
+   exec(command1+ cmd_params + log_cmd,function(error, stdout, stderr){
+
+  console.log('stdout: ' + stdout);
+  console.log('!!!!!!!!!!!!!!: ');
+  console.log('stderr: ' + stderr);
+ //   var err = new Error( stderr);
+ 
+ // console.log("done error");
+ // console.log(  err   );
+
+var output = fs.readFileSync(job_file).toString().split("\n");
+
+var len = output.length
+
+
+var status = output[len-2];
+
+
+
+      if (status =='completed')
+    {
+      console.log(  status        );
+      done()
+
+      // console.log('stderr: ' + stderr);
+     
+     
+      } 
+      else
+     {
+      var err = new Error( stderr);
+      done(err);
+       console.log("done error");
+
+     }
+
+
+
+
+
+   
+
+
+
+
+// if (error) {
+//    console.error("error:   "+ error);
+//     console.error(`exec error: ${error}`);
+//     done(error);
+//   }
+
+//   else
+//   {
+//     console.log("no error");
+//     done();
+//   }
+  
+
+
+
+
+
+
+
+
+
+    
   });
+
+
+
+ // var domain = require('domain').create();
+ //  domain.on('error', function(err){
+ //    done(err);
+ //  });
+ //  domain.run(function(){ 
+
+ //    exec(command1+ cmd_params+log_cmd,function(error, stdout, stderr){
+ //    if (stdout.length > 0) console.log('stdout: ' + stdout);
+ //    if (stderr.length > 0) console.log('stderr: ' + stderr);
+ //    done();
+ //  });
+ //  });
+
+
 
 
 });
@@ -60,6 +163,8 @@ console.log(data_body);
 
 var params_array = ['algos','caseids','metric','input','output','result_exe_id']
 
+
+// var params_array = ['caseids','metric','input','output','result_exe_id']
 var cmd_params ='';
 
 
@@ -83,23 +188,28 @@ console.log(cmd_params);
 
 var unique_title = uuid.v1();
 
+var job_id;
 
- queue.create('heat_map', {
-    title: 'heat_map'+'_'+unique_title
+ var job = queue.create('heat_map', {
+    title: unique_title
   , cmd_params:  cmd_params
   
-}).save();
+}).save( function(){
+    job_id = job.id ;
+    console.log("unique_job_id: "+job_id)
+    res.send('job_id: '+job_id);
+});
 
 
 
 
-res.send(unique_title);
+
 
 });
 
 
 
-var server = app.listen(8093, function () {
+var server = app.listen(8099, function () {
 
   var host = server.address().address
   var port = server.address().port
