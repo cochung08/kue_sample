6a6b1bb10174e8d759746b7817d1b27eddc0023f
cheuk <cochung1993@gmail.com>
2016-06-22 10:42:17 -0400

    sd

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var kue = require('kue');
 var queue = kue.createQueue({
-    disableSearch: false
+  disableSearch: false
 });
 
 
@@ -73,43 +73,43 @@ var find_heat_map_by_jobid = function(db, job_id,callback) {
 app.post('/api/get_heat_map_result', function(req, res) {
 
 
-var data_body = req.body;
+  var data_body = req.body;
 
-console.log(data_body);
+  console.log(data_body);
 
-var key = 'jobid'
-
-
-    var  job_id = data_body[key];
-console.log(job_id);
+  var key = 'jobid'
 
 
+  var  job_id = data_body[key];
+  console.log(job_id);
 
 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
+
+
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
 
 
     var cursor =db.collection('ad_hoc_results').find(  {   "job_id":  job_id }      );
-   cursor.toArray(function (err, results) {
-   	 db.close();
+    cursor.toArray(function (err, results) {
+     db.close();
 
-   	 var result_json = {}
+     var result_json = {}
 
-   	 for(var i=0;i< results.length; i++)
-   	 {
-   	 	result_json[i] =   results[i]
-   	 }
-
-	    if (!err) {
-	    	console.log(result_json)
-	    	res.send('results: '+JSON.stringify (results));
-    
+     for(var i=0;i< results.length; i++)
+     {
+      result_json[i] =   results[i]
     }
-});
+
+    if (!err) {
+      console.log(result_json)
+      res.send('results: '+JSON.stringify (results));
+      
+    }
+  });
 
 
-});
+  });
 
 
 
@@ -124,43 +124,43 @@ app.post('/api/upload', function(req, res) {
 
 
 
-    // var  files = req.files.image;
-   
-console.log("sdasd")
+      // var  files = req.files.image;
+      
+      console.log("sdasd")
 
- var form = new multiparty.Form();
- 
-    form.parse(req, function(err, fields, files) {
+      var form = new multiparty.Form();
+      
+      form.parse(req, function(err, fields, files) {
        console.log(files.image);
-      var ele =  files.image
+       var ele =  files.image
        var tmp_path = ele[0].path
 
        console.log(tmp_path)
 
        var obj = fs.readFileSync(tmp_path ,'utf8');
-          console.log(obj);
+       console.log(obj);
 
 
-var command1 ="mongoimport --host localhost:27017 --db u24_segmentation --collection results --file " + tmp_path
+       var command1 ="mongoimport --host localhost:27017 --db u24_segmentation --collection results --file " + tmp_path
 
 
-exec(command1,function(error, stdout, stderr){
+       exec(command1,function(error, stdout, stderr){
 
-  });
+       });
 
- res.send('xxx '+"uni_job_id");
+       res.send('xxx '+"uni_job_id");
+     });
+
+      // form.on('field', function(name, val){
+      //      console.log(val);
+      //      });
+
+
+
+
+
+
     });
-
-    // form.on('field', function(name, val){
-    //      console.log(val);
-    //      });
-
-
-
-
-
-
-});
 
 
 
@@ -169,10 +169,10 @@ exec(command1,function(error, stdout, stderr){
 
 queue.on( 'error', function( err ) {
   console.log( 'Oops... ', err );
-   console.log( 'Oops... ', err );
-    console.log( 'Oops... ', err );
-     console.log( 'Oops... ', err );
-      console.log( 'Oops... ', err );
+  console.log( 'Oops... ', err );
+  console.log( 'Oops... ', err );
+  console.log( 'Oops... ', err );
+  console.log( 'Oops... ', err );
 });
 
 
@@ -182,7 +182,7 @@ queue.process('heat_map',2, function(job, done){
 
 
 
-var spark_path = "/home/cochung/spark_full" ;
+  var spark_path = "/home/cochung/spark_full" ;
 
 
 
@@ -197,41 +197,41 @@ var spark_path = "/home/cochung/spark_full" ;
   var log_cmd = " > "+job_file
 
 
-   exec(command1+ cmd_params + log_cmd,function(error, stdout, stderr){
+  exec(command1+ cmd_params + log_cmd,function(error, stdout, stderr){
 
-  console.log('stdout: ' + stdout);
-  console.log('!!!!!!!!!!!!!!: ');
-  console.log('stderr: ' + stderr);
-
-
-var output = fs.readFileSync(job_file).toString().split("\n");
-
-var len = output.length
+    console.log('stdout: ' + stdout);
+    console.log('!!!!!!!!!!!!!!: ');
+    console.log('stderr: ' + stderr);
 
 
-var status = output[len-2];
+    var output = fs.readFileSync(job_file).toString().split("\n");
+
+    var len = output.length
+
+
+    var status = output[len-2];
 
 
 
-      if (status =='completed')
+    if (status =='completed')
     {
       console.log(  status        );
       done()
 
-      // console.log('stderr: ' + stderr);
-     
-     
+        // console.log('stderr: ' + stderr);
+        
+        
       } 
       else
-     {
-      var err = new Error( stderr);
-      done(err);
-       console.log("done error");
+      {
+        var err = new Error( stderr);
+        done(err);
+        console.log("done error");
 
-     }
+      }
 
-  
-  });
+      
+    });
 
 
 
@@ -248,57 +248,55 @@ var status = output[len-2];
 app.post('/api/get_heat_map', function(req, res) {
 
 
-var data_body = req.body;
+  var data_body = req.body;
 
-console.log(data_body);
+  console.log(data_body);
 
-var params_array = ['uid','algos','caseids','metric','input','output','result_exe_id']
-
-
-// var params_array = ['caseids','metric','input','output','result_exe_id']
-var cmd_params ='';
+  var params_array = ['uid','algos','caseids','metric','input','output','result_exe_id']
 
 
-
-for(var key of params_array)
-{
-
-    // console.log("key: "+key)
-    // console.log('value: '+value);
-
-    var  value = data_body[key];
-    var tmp = ' --'+key+' '+value;
-
-    cmd_params = cmd_params+tmp;
-}
-
-console.log(cmd_params);
+  // var params_array = ['caseids','metric','input','output','result_exe_id']
+  var cmd_params ='';
 
 
 
+  for(var key of params_array)
+  {
 
-var uni_job_id = uuid.v1();
+      // console.log("key: "+key)
+      // console.log('value: '+value);
 
-var job_id;
+      var  value = data_body[key];
+      var tmp = ' --'+key+' '+value;
 
- var job = queue.create('heat_map', {
-    title: uni_job_id
-  , cmd_params:  cmd_params
-  
-}).save( function(){ 
-  job.uni_job_id = uni_job_id
-    job_id = job.id ;
-    console.log("unique_job_id: "+uni_job_id)
+      cmd_params = cmd_params+tmp;
+    }
 
-
-
-update_job_status(job)
+    console.log(cmd_params);
 
 
-    res.send('job_id: '+uni_job_id);
-});
 
 
+    var uni_job_id = uuid.v1();
+
+    var job_id;
+
+    var job = queue.create('heat_map', {
+      title: uni_job_id
+      , cmd_params:  cmd_params
+      
+    }).save( function(){ 
+      job.uni_job_id = uni_job_id
+      job_id = job.id ;
+      console.log("unique_job_id: "+uni_job_id)
+
+
+
+      update_job_status(job)
+
+
+      res.send('job_id: '+uni_job_id);
+    });
 
 
 
@@ -321,7 +319,9 @@ update_job_status(job)
 
 
 
-});
+
+
+  });
 
 
 
@@ -329,23 +329,23 @@ var insert_status_with_jobid = function(status,jobid)
 {
 
   MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
+    assert.equal(null, err);
 
 
 
- db.collection('ad_hoc_results').insertOne( {
+    db.collection('ad_hoc_results').insertOne( {
       "jobid" : jobid,
       "status" :status
       
-    
-   }, function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted a document into the restaurants collection.");
+      
+    }, function(err, result) {
+      assert.equal(err, null);
+      console.log("Inserted a document into the restaurants collection.");
       db.close();
+    });
+
+
   });
-
-
-});
 
 }
 
@@ -359,21 +359,21 @@ var update_status_by_jobid = function(status,jobid)
 {
 
   MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
+    assert.equal(null, err);
 
 
-db.collection('ad_hoc_results').updateOne(
+    db.collection('ad_hoc_results').updateOne(
       {"jobid" : jobid },
       {
         $set: {  "status" :status },
         $currentDate: { "lastModified": true }
       }, function(err, results) {
-      console.log("updated");
-      db.close();
-   });
+        console.log("updated");
+        db.close();
+      });
 
 
-});
+  });
 
 }
 
@@ -384,57 +384,57 @@ db.collection('ad_hoc_results').updateOne(
 
 
 var updateStatusByJobId = function(db,status,jobid, callback) {
-   db.collection('ad_hoc_results').updateOne(
-      {"jobid" : jobid },
-      {
-        $set: {  "status" :status },
-        $currentDate: { "lastModified": true }
-      }, function(err, results) {
-      console.log(results);
-      callback();
-   });
+ db.collection('ad_hoc_results').updateOne(
+  {"jobid" : jobid },
+  {
+    $set: {  "status" :status },
+    $currentDate: { "lastModified": true }
+  }, function(err, results) {
+    console.log(results);
+    callback();
+  });
 };
 
 var update_job_status= function(job)
 {
 
-var uni_job_id = job.uni_job_id ;
+  var uni_job_id = job.uni_job_id ;
 
-console.log("uni_job_id:   "+uni_job_id)
+  console.log("uni_job_id:   "+uni_job_id)
 
-job.on('complete', function(result){
-  // console.log('Job completed with data ', result);
+  job.on('complete', function(result){
+    // console.log('Job completed with data ', result);
 
-update_status_by_jobid('complete',uni_job_id)
-
-
-
-
-}).on('failed attempt', function(errorMessage, doneAttempts){
-  console.log('Job failed');
-
-}).on('start', function(errorMessage, doneAttempts){
-  console.log('Job started');
+    update_status_by_jobid('complete',uni_job_id)
 
 
 
-update_status_by_jobid('start',uni_job_id)
+
+  }).on('failed attempt', function(errorMessage, doneAttempts){
+    console.log('Job failed');
+
+  }).on('start', function(errorMessage, doneAttempts){
+    console.log('Job started');
 
 
 
-}).on('enqueue', function(result){
+    update_status_by_jobid('start',uni_job_id)
 
-  console.log('enqueue12');
 
-insert_status_with_jobid('enqueue',uni_job_id)
-// update_status_by_jobid('enqueue',uni_job_id)
+
+  }).on('enqueue', function(result){
+
+    console.log('enqueue12');
+
+    insert_status_with_jobid('enqueue',uni_job_id)
+  // update_status_by_jobid('enqueue',uni_job_id)
 
   
 
 }).on('progress', function(progress, data){
-  // console.log('\r  job #' + job.id + ' ' + progress + '% complete with data ', data );
-console.log("ssssssssss")
-});
+    // console.log('\r  job #' + job.id + ' ' + progress + '% complete with data ', data );
+    console.log("ssssssssss")
+  });
 }
 
 
@@ -458,18 +458,18 @@ var server = app.listen(8127, function () {
 
 
 
-// ssssssssssssssssssss
+  // ssssssssssssssssssss
 
 
 
 
-var insertStatusByJobId = function(db,status, jobid,callback) {
+  var insertStatusByJobId = function(db,status, jobid,callback) {
    db.collection('ad_hoc_results').insertOne( {
-      "jobid" : jobid,
-      "status" :status
-      
+    "jobid" : jobid,
+    "status" :status
     
-   }, function(err, result) {
+    
+  }, function(err, result) {
     assert.equal(err, null);
     console.log("Inserted a document into the restaurants collection.");
     callback();
@@ -478,19 +478,19 @@ var insertStatusByJobId = function(db,status, jobid,callback) {
 
 
 
-}
+ }
 
 
 
 
-var updateStatusByJobId = function(db,status,jobid, callback) {
+ var updateStatusByJobId = function(db,status,jobid, callback) {
    db.collection('ad_hoc_results').updateOne(
-      {"jobid" : jobid },
-      {
-        $set: {  "status" :status },
-        $currentDate: { "lastModified": true }
-      }, function(err, results) {
+    {"jobid" : jobid },
+    {
+      $set: {  "status" :status },
+      $currentDate: { "lastModified": true }
+    }, function(err, results) {
       console.log(results);
       callback();
-   });
-};
+    });
+ };
